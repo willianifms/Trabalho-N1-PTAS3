233b1fa27ba83cdf0e5817e51076b9781cdc4493
willianifms <williancardoso00004@gmail.com>
2023-09-12 10:36:27 -0400

    autentica users

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,29 +68,28 @@ const updateUser = async (req, res) => {
 }
 
 const authenticatedUser = async (req, res) => {
-    const {       } = req.body;
+    const { email, password } = req.body;
     try {
         const isUserAuthenticated = await User.findOne({
             where: {
-                
+                email: email,
+                password: password
             }
         })
         const token = jwt.sign({
-            name: 
-            email: 
+            name: isUserAuthenticated.name,
+            email: isUserAuthenticated.email
         },
             secret.secret, {
             expiresIn: 86400,
         })
         return res.json({
-            name: 
+            name: isUserAuthenticated.name,
             email: isUserAuthenticated.email,
             token: token
         });
     } catch (error) {
-        return res.json("");
+        return res.json("Erro na autenticação.");
     }
 }
-
-
 module.exports = { createUser, findUsers, deleteUser, updateUser, authenticatedUser };
